9e53e272cc9086091936e7a3d5e0d77e097c9ae6
Andrew <AndriyKriminskij@gmail.com>
2023-05-29 19:25:54 +0300

    modofied Jenkins script

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -2,7 +2,11 @@ sudo apt update
 
 sudo apt install -y openjdk-11-jdk
 
-wget -qO - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo apt update
 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
