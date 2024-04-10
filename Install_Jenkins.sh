9f091854174b9cad61587f4147a94774db84695c
Andrew <134720993+AndrewKrmn@users.noreply.github.com>
2024-04-10 19:17:39 +0300

    Update Install_Jenkins.sh

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -1,21 +1,14 @@
-sudo apt update
-
+sudo apt update 
+sudo apt upgrade -y 
 sudo apt install -y openjdk-11-jdk
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
-
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-
-sudo apt update
-
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update
-
-sudo apt install -y jenkins
-
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+  sudo apt-get install fontconfig openjdk-17-jre
+  sudo apt-get install jenkins
 sudo systemctl start jenkins
-
 sudo systemctl enable jenkins
-
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
